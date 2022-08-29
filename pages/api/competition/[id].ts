3c4b0348593f8c079ca9c6f7f9ae9b4f6204c8e6
tohsaka888 <156132264@qq.com>
2022-08-29 11:45:59 +0800

    ✨ feat(api): 获取比赛详情

---
+++ pages/api/competition/[id].ts
@@ -0,0 +1,42 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { ObjectId } from 'mongodb'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { connectDB } from 'utils/connectDB'
+
+/**
+ * @openapi
+ * paths:
+ *   /api/competition/{id}:
+ *     get:
+ *       description: 获取比赛简单列表
+ *       parameters:
+ *         - in: path
+ *           name: id
+ *           required: true
+ *           example: 630c28a9738c38ca68927c8a
+ *           schema:
+ *              type: string
+ *       responses:
+ *         200:
+ *           description: 返回比赛详情
+ *               
+ */
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const db = await connectDB()
+    const query = req.query
+    if (db) {
+      const competitionCollection = db.collection('competition')
+      const competition = await competitionCollection.findOne({_id: new ObjectId(query.id as string)})
+      res.status(200).json({ success: true, competition })
+    } else {
+      new Error('连接数据库失败')
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: (error as Error).message })
+  }
+}
