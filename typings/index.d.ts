caf8e2c8d912d9c6c18cd9e4d0afdd6a42b5c509
tohsaka888 <156132264@qq.com>
2022-08-29 11:16:09 +0800

    ✨ feat: 比赛type

---
+++ typings/index.d.ts
@@ -0,0 +1,47 @@
+declare namespace API {
+
+  type Participant = {
+    username: string;
+    email: string;
+  }
+
+  type CompetitionImage = {
+    intro: string;
+    url: string;
+  }
+
+  type Award = {
+    award: string;
+  }
+
+  type AwardSetting = {
+    award: string;
+    limit: number;
+  }
+
+  type Winner = Participant & Award;
+
+  type Creator = Participant;
+
+  type Info = {
+    place: string;
+    way: string;
+    limit: number;
+    signUpStart: string;
+    signUpEnd: string;
+    time: string;
+    duration: string;
+  }
+
+  type Competition = {
+    name: string;
+    createdTime: string;
+    updatedTime: string;
+    participants: Participant[];
+    winners: Winner[];
+    info: Info;
+    banners: CompetitionImage[];
+    creator: Creator;
+    awardSetting: AwardSetting[];
+  }
+}
