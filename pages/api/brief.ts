05d11bc0de4cde0da9fc85ea943c5a90d3da6f73
tohsaka888 <156132264@qq.com>
2022-08-29 11:15:21 +0800

    ✨ feat(api): 比赛列表

---
+++ pages/api/brief.ts
@@ -0,0 +1,51 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { connectDB } from '../../utils/connectDB'
+
+/**
+ * @openapi
+ * /api/brief:
+ *   post:
+ *     description: 获取比赛简单列表
+ *     responses:
+ *       200:
+ *         description: 返回列表
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success: 
+ *                   type: boolean
+ *                 list: 
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: string
+ *                         example: axxss
+ *                       name:
+ *                         type: string
+ *                         example: 比赛
+ *               
+ */
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const db = await connectDB()
+    if (db) {
+      const competition = db.collection('competition')
+      const competitions = await competition.find({}).toArray()
+      const briefList = competitions.map((item) => ({ id: item._id, name: item.name }))
+      res.status(200).json({ success: true, list: briefList })
+    } else {
+      new Error('连接数据库失败')
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, error: (error as Error).message })
+  }
+}
